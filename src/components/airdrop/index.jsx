1c23801070e5c9ae53c70b4216e16e3ecb2f7b97
victor <victor@pt-corp.vn>
2023-08-08 22:57:01 +0700

    fix loading

--- src/components/airdrop/index.jsx
+++ src/components/airdrop/index.jsx
@@ -119,248 +119,244 @@ const AirdropPage = () => {
           pb={4}
         >
           <>
-            {isLoading ? (
-              <CicularLoading />
-            ) : (
-              <>
-                <Text
-                  fontSize={{ base: "2xl", md: "5xl" }}
-                  textAlign="center"
-                  color={"#5EEDFF"}
-                >
-                  PIRA SEASON 1
-                </Text>
+            {isLoading && <CicularLoading />}
+            <>
+              <Text
+                fontSize={{ base: "2xl", md: "5xl" }}
+                textAlign="center"
+                color={"#5EEDFF"}
+              >
+                PIRA SEASON 1
+              </Text>
 
-                <Progress />
+              <Progress />
 
-                <Text
-                  fontSize={{ base: "1xl", md: "3xl" }}
-                  textAlign="center"
+              <Text
+                fontSize={{ base: "1xl", md: "3xl" }}
+                textAlign="center"
+                color="white"
+                my={6}
+              >
+                All transaction fees will be used for buyback when the token is
+                listed on CEX
+              </Text>
+
+              <SimpleGrid
+                columns={{ base: 1, md: 2, lg: 3 }}
+                width={{ base: "full", lg: "95%" }}
+                marginX="auto"
+                spacing={10}
+                paddingX={{ base: 6, md: 0 }}
+              >
+                <Box
+                  rounded="lg"
+                  borderWidth={2}
+                  borderColor={"cyan.400"}
                   color="white"
-                  my={6}
+                  bg="blue.900"
+                  role="group"
                 >
-                  All transaction fees will be used for buyback when the token
-                  is listed on CEX
-                </Text>
-
-                <SimpleGrid
-                  columns={{ base: 1, md: 2, lg: 3 }}
-                  width={{ base: "full", lg: "95%" }}
-                  marginX="auto"
-                  spacing={10}
-                  paddingX={{ base: 6, md: 0 }}
+                  <Flex
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    h="100%"
+                  >
+                    <HStack>
+                      <Text fontSize={{ base: "1xl", md: "3xl" }}>
+                        Earning Token
+                      </Text>
+                      <Popover trigger="hover">
+                        <PopoverTrigger>
+                          <InfoOutlineIcon color={"#00F0FF"} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                          width={"194px"}
+                          height={"60px"}
+                          border="none"
+                        >
+                          <Box
+                            rounded="lg"
+                            borderWidth={1}
+                            borderColor={"cyan.400"}
+                            color="white"
+                            bg="blue.600"
+                            role="group"
+                          >
+                            <Text
+                              color={"white"}
+                              padding={"5px"}
+                              textAlign={"center"}
+                            >
+                              Number of points you earn through completing task
+                            </Text>
+                          </Box>
+                        </PopoverContent>
+                      </Popover>
+                    </HStack>
+                    <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
+                      {overview?.earningTokens}
+                    </Text>
+                  </Flex>
+                </Box>
+                <Box
+                  rounded="lg"
+                  borderWidth={2}
+                  borderColor={"cyan.400"}
+                  color="white"
+                  bg="blue.900"
+                  role="group"
+                  justifyContent={"center"}
                 >
-                  <Box
-                    rounded="lg"
-                    borderWidth={2}
-                    borderColor={"cyan.400"}
-                    color="white"
-                    bg="blue.900"
-                    role="group"
+                  <Flex
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    h="100%"
                   >
-                    <Flex
-                      direction="column"
-                      justifyContent="center"
-                      alignItems="center"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Text fontSize={{ base: "1xl", md: "3xl" }}>
-                          Earning Token
-                        </Text>
-                        <Popover trigger="hover">
-                          <PopoverTrigger>
-                            <InfoOutlineIcon color={"#00F0FF"} />
-                          </PopoverTrigger>
-                          <PopoverContent
-                            width={"194px"}
-                            height={"60px"}
-                            border="none"
-                          >
-                            <Box
-                              rounded="lg"
-                              borderWidth={1}
-                              borderColor={"cyan.400"}
-                              color="white"
-                              bg="blue.600"
-                              role="group"
-                            >
-                              <Text
-                                color={"white"}
-                                padding={"5px"}
-                                textAlign={"center"}
-                              >
-                                Number of points you earn through completing
-                                task
-                              </Text>
-                            </Box>
-                          </PopoverContent>
-                        </Popover>
-                      </HStack>
-                      <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
-                        {overview?.earningTokens}
+                    <HStack>
+                      <Text fontSize={{ base: "1xl", md: "3xl" }}>
+                        Referral Token
                       </Text>
-                    </Flex>
-                  </Box>
-                  <Box
-                    rounded="lg"
-                    borderWidth={2}
-                    borderColor={"cyan.400"}
-                    color="white"
-                    bg="blue.900"
-                    role="group"
-                    justifyContent={"center"}
-                  >
-                    <Flex
-                      direction="column"
-                      justifyContent="center"
-                      alignItems="center"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Text fontSize={{ base: "1xl", md: "3xl" }}>
-                          Referral Token
-                        </Text>
-                        <Popover trigger="hover">
-                          <PopoverTrigger>
-                            <InfoOutlineIcon color={"#00F0FF"} />
-                          </PopoverTrigger>
-                          <PopoverContent
-                            width={"194px"}
-                            height={"60px"}
-                            border="none"
+                      <Popover trigger="hover">
+                        <PopoverTrigger>
+                          <InfoOutlineIcon color={"#00F0FF"} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                          width={"194px"}
+                          height={"60px"}
+                          border="none"
+                        >
+                          <Box
+                            rounded="lg"
+                            borderWidth={1}
+                            borderColor={"cyan.400"}
+                            color="white"
+                            bg="blue.600"
+                            role="group"
                           >
-                            <Box
-                              rounded="lg"
-                              borderWidth={1}
-                              borderColor={"cyan.400"}
-                              color="white"
-                              bg="blue.600"
-                              role="group"
+                            <Text
+                              color={"white"}
+                              padding={"5px"}
+                              textAlign={"center"}
                             >
-                              <Text
-                                color={"white"}
-                                padding={"5px"}
-                                textAlign={"center"}
-                              >
-                                Number of points you earn inviting friends
-                              </Text>
-                            </Box>
-                          </PopoverContent>
-                        </Popover>
-                      </HStack>
-                      <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
-                        {overview?.referralTokens}
-                      </Text>
-                    </Flex>
-                  </Box>
-                  <Box
-                    rounded="lg"
-                    borderWidth={2}
-                    borderColor={"cyan.400"}
-                    color="white"
-                    bg="blue.900"
-                    role="group"
-                  >
-                    <Flex
-                      direction="column"
-                      justifyContent="center"
-                      alignItems="center"
-                      h="100%"
-                    >
-                      <HStack>
-                        <Text fontSize={{ base: "1xl", md: "3xl" }}>BOOST</Text>
-                        <Popover trigger="hover">
-                          <PopoverTrigger>
-                            <InfoOutlineIcon color={"#00F0FF"} />
-                          </PopoverTrigger>
-                          <PopoverContent
-                            width={"200px"}
-                            height={"54px"}
-                            border="none"
-                          >
-                            <Box
-                              rounded="lg"
-                              borderWidth={1}
-                              borderColor={"cyan.400"}
-                              color="white"
-                              bg="blue.600"
-                              role="group"
-                            >
-                              <Text
-                                color={"white"}
-                                padding={"5px"}
-                                textAlign={"center"}
-                              >
-                                Inviting 2 people will boost you 1.5x, inviting
-                                5 people will boost you 2x
-                              </Text>
-                            </Box>
-                          </PopoverContent>
-                        </Popover>
-                      </HStack>
-                      <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
-                        {overview?.boost}x
-                      </Text>
-                    </Flex>
-                  </Box>
-                </SimpleGrid>
-
-                <VStack spacing="24px" mt={14}>
-                  <Flex justify="center">
-                    <Text
-                      rounded="lg"
-                      borderWidth={2}
-                      borderColor="cyan.400"
-                      fontSize={{ base: "1xl", md: "2xl" }}
-                      px={24}
-                      py={2}
-                      bg="white"
-                    >
-                      Pira Season 1 detail
+                              Number of points you earn inviting friends
+                            </Text>
+                          </Box>
+                        </PopoverContent>
+                      </Popover>
+                    </HStack>
+                    <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
+                      {overview?.referralTokens}
                     </Text>
                   </Flex>
-
-                  <Flex width={"full"} flexWrap="nowrap">
-                    <Text
-                      fontSize={{ base: "1xl", md: "2xl" }}
-                      whiteSpace={"nowrap"}
-                      overflow={"hidden"}
-                      textOverflow="ellipsis"
-                      maxWidth={600}
-                      textAlign="start"
-                      color="white"
-                    >
-                      Referral link: {`${FE_DOMAIN}/airdrop?ref=${address}`}
+                </Box>
+                <Box
+                  rounded="lg"
+                  borderWidth={2}
+                  borderColor={"cyan.400"}
+                  color="white"
+                  bg="blue.900"
+                  role="group"
+                >
+                  <Flex
+                    direction="column"
+                    justifyContent="center"
+                    alignItems="center"
+                    h="100%"
+                  >
+                    <HStack>
+                      <Text fontSize={{ base: "1xl", md: "3xl" }}>BOOST</Text>
+                      <Popover trigger="hover">
+                        <PopoverTrigger>
+                          <InfoOutlineIcon color={"#00F0FF"} />
+                        </PopoverTrigger>
+                        <PopoverContent
+                          width={"200px"}
+                          height={"54px"}
+                          border="none"
+                        >
+                          <Box
+                            rounded="lg"
+                            borderWidth={1}
+                            borderColor={"cyan.400"}
+                            color="white"
+                            bg="blue.600"
+                            role="group"
+                          >
+                            <Text
+                              color={"white"}
+                              padding={"5px"}
+                              textAlign={"center"}
+                            >
+                              Inviting 2 people will boost you 1.5x, inviting 5
+                              people will boost you 2x
+                            </Text>
+                          </Box>
+                        </PopoverContent>
+                      </Popover>
+                    </HStack>
+                    <Text fontSize={{ base: "2xl", md: "4xl" }} my={4}>
+                      {overview?.boost}x
                     </Text>
-                    <CopyIcon
-                      onClick={copyRefLink}
-                      fontSize="2xl"
-                      color="white"
-                      cursor="pointer"
-                      mx="2px"
-                    />
                   </Flex>
+                </Box>
+              </SimpleGrid>
 
-                  <Flex width="full">
-                    <Text
-                      fontSize={{ base: "1xl", md: "3xl" }}
-                      borderBottomWidth={2}
-                      borderColor={"cyan.400"}
-                      color="white"
-                    >
-                      How to get PIRA?
-                    </Text>
-                  </Flex>
-                  <TaskTable
-                    copyRefLink={copyRefLink}
-                    handleMintNFT={handleMintNFT}
-                    handleClaim={handleClaim}
+              <VStack spacing="24px" mt={14}>
+                <Flex justify="center">
+                  <Text
+                    rounded="lg"
+                    borderWidth={2}
+                    borderColor="cyan.400"
+                    fontSize={{ base: "1xl", md: "2xl" }}
+                    px={24}
+                    py={2}
+                    bg="white"
+                  >
+                    Pira Season 1 detail
+                  </Text>
+                </Flex>
+
+                <Flex width={"full"} flexWrap="nowrap">
+                  <Text
+                    fontSize={{ base: "1xl", md: "2xl" }}
+                    whiteSpace={"nowrap"}
+                    overflow={"hidden"}
+                    textOverflow="ellipsis"
+                    maxWidth={600}
+                    textAlign="start"
+                    color="white"
+                  >
+                    Referral link: {`${FE_DOMAIN}/airdrop?ref=${address}`}
+                  </Text>
+                  <CopyIcon
+                    onClick={copyRefLink}
+                    fontSize="2xl"
+                    color="white"
+                    cursor="pointer"
+                    mx="2px"
                   />
-                  <LeaderBoard />
-                </VStack>
-              </>
-            )}
+                </Flex>
+
+                <Flex width="full">
+                  <Text
+                    fontSize={{ base: "1xl", md: "3xl" }}
+                    borderBottomWidth={2}
+                    borderColor={"cyan.400"}
+                    color="white"
+                  >
+                    How to get PIRA?
+                  </Text>
+                </Flex>
+                <TaskTable
+                  copyRefLink={copyRefLink}
+                  handleMintNFT={handleMintNFT}
+                  handleClaim={handleClaim}
+                />
+                <LeaderBoard />
+              </VStack>
+            </>
           </>
         </Container>
       )}
