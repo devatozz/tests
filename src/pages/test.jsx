70c4adb52ba650c07ed200c9ec6fb1eb0282e4df
Kai <Haunv1503@gmail.com>
2024-01-15 21:14:18 +0700

    Delete test.jsx

--- src/pages/test.jsx
+++
@@ -1,7 +0,0 @@
-import TradingViewWidget from "src/components/home/PriceSlider";
-
-export default function test() {
-    return <div style={{ backgroundColor: '#21281a', width: '100%', margin: 'auto', padding: '0 10%' }}>
-        <TradingViewWidget />
-    </div>
-}
