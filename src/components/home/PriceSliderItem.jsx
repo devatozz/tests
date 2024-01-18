18b4c43361c5f8952cb1ad0ea183950cbf2417c6
Kai <Haunv1503@gmail.com>
2024-01-18 17:07:15 +0700

    Update PriceSliderItem.jsx

--- src/components/home/PriceSliderItem.jsx
+++ src/components/home/PriceSliderItem.jsx
@@ -98,7 +98,7 @@ export default function PriceSliderItem({ tokenData }) {
                 justifyContent={{ base: "center", xl: "flex-start" }}
                 alignItems={"center"}
                 justifyItems={"center"}
-                gap={"10px"}
+                gap={{base: "7px", md: "10px"}}
                 height={{ base: "70px", xl: "80px" }}
             >
                 <Image width={{ base: "50px", xl: "60px" }} height={{ base: "50px", xl: "60px" }} borderRadius={"50%"} p={"5px"} src={token.image} alt={token.name} />
